332f4f3ef7b0c652044386c1d775930d95b55efb
Vida Zarei <vida1997zarei@gmail.com>
2024-09-25 08:53:09 -0400

    UI for Landinsection is applied

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -7,6 +7,7 @@ const bio1 = "A frontend developer";
 const bio2 = "specialised in React";
 
 // Implement the UI for the LandingSection component according to the instructions.
+
 // Use a combination of Avatar, Heading and VStack components.
 const LandingSection = () => (
   <FullScreenSection
@@ -15,7 +16,18 @@ const LandingSection = () => (
     isDarkBackground
     backgroundColor="#2A4365"
   >
-
+    <VStack spacing={4}>
+      <Avatar size="2xl" src="https://i.pravatar.cc/150?img=7" />
+      <Heading as="h1" size="xl">
+        {greeting}
+      </Heading>
+      <Heading as="h2" size="md">
+        {bio1}
+      </Heading>
+      <Heading as="h2" size="md">
+        {bio2}
+      </Heading>
+    </VStack>
   </FullScreenSection>
 );
 
